8161791ddc53092883ed14d2102c062c7ef14d71
James Johnson <jcejohnson@users.noreply.github.com>
2015-05-31 10:55:20 -0400

    Trying to get runit to work with Docker

--- jenkins-cookbook/metadata.rb
+++ jenkins-cookbook/metadata.rb
@@ -5,5 +5,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 
-depends "jenkins", "~> 2.3.0"
+depends "jenkins", "~> 2.3.1"
 depends "runit", "~> 1.5"
--- jenkins-cookbook/test/fixtures/cookbooks/runit-helper/recipes/default.rb
+++ jenkins-cookbook/test/fixtures/cookbooks/runit-helper/recipes/default.rb
@@ -16,10 +16,13 @@ log("Launching runit with #{runsvdir} ...")
 # This is gross (also see .kitchen.yml). I just need runit to be up and running
 # for the test / example. You wouldn't want to do this in a real cookbook.
 bash 'launch-runit' do
-	code <<-EOF
-		nohup #{node['runit_helper'][platform]['start']} 2>&1 &
-		sleep 15
-	EOF
+    code <<-EOF
+        killall -0 runsvdir || (
+            nohup #{node['runit_helper'][platform]['start']} 2>&1 &
+            sleep 15
+            ps -aef > /tmp/ps-aef
+        )
+    EOF
 end
 
 log("runit should be ready to go")
